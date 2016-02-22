ecd54e3e660f4124d761c5999121c51256a2a96c
Clodéric Mars <cloderic.mars@craft.ai>
2016-02-22 12:55:36 +0100

    Initial version using the style from craft-ai/craft-ai-client-js

---
+++ README.md
@@ -0,0 +1,4 @@
+# eslint-config-craft-ai #
+
+This is [craft ai](https://craft.ai/)'s [ESLint](http://eslint.org/)
+configuration for es5 and es6.
---
+++ rules.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "rules": {
+    "indent": [
+      2,
+      2,
+      {"SwitchCase": 1}
+    ],
+    "quotes": [
+      2,
+      "single"
+    ],
+    "semi": [
+      2,
+      "always"
+    ],
+    "no-console": [
+      0
+    ],
+    "no-unused-vars": [
+      1,
+      {
+        "args": "none"
+      }
+    ],
+    "default-case": 2
+  },
+  "ecmaFeatures": {
+    "modules": true
+  },
+  "env": {
+    "es6": true,
+    "node": true,
+    "browser": true
+  }
+};
