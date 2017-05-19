1f80c288516f3b21ba1c1ac3c0f917541a320d00
alexisfontaine <alexis.fontaine@isep.fr>
2017-05-19 15:27:49 +0200

    Update configuration

--- react.babel.js
+++ react.babel.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: [
-    './react'
-  ].map(require.resolve)
+  extends: ['./react'].map(require.resolve),
+  parserOptions: {
+    sourceType: 'module'
+  }
 };
